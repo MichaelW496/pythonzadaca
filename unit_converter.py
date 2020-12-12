2352365302741a1a72082d8157a51d2db05f2db8
Michael Wendler <michaelwendler9@gmail.com>
2020-12-13 00:03:14 +0100

    adding a "thank you" in the end

--- unit_converter.py
+++ unit_converter.py
@@ -1,10 +1,10 @@
 print("Hello! This program converts kilometers into miles.")
 
-
 while True:
     Km = int(input("Enter the number of kilometers: "))
     Miles = (Km * 1.6)
-    print(str(Km) + " kilometers is " + str(Miles) + " miles")
-    additional = input("Would you like another query (y/n): ")
-    if additional != "y":
+    print(str(Km) + " kilometers is " + str(Miles) + " miles.")
+    additional = input("Would you like to do another conversion? ")
+    if additional.lower() != "y" and additional.lower() != "yes":
+        print("Thank you for using this converter.")
         break
